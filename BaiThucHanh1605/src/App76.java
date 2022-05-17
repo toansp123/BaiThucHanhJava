ced51766c0cccc1ab1156833d83a6f1e37c1d9aa
toansp123 <97439368+toansp123@users.noreply.github.com>
2022-05-17 22:09:20 +0700

    HuyToan-2021050903

---
+++ BaiThucHanh1605/src/App76.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.TreeMap;
+
+
+public class App76 {
+    public static void main(String[] args) {
+        
+        HashMap<Integer, Float> hashMap = new HashMap<>();
+        HashMap<String , String> hashMap1 =new HashMap<>(10);
+        HashMap<Double, Double> hashMap2 = new HashMap<>(4, 0.75f);
+        HashMap<Float, Integer> hashMap3 = new HashMap<>(new TreeMap<>());
+    }
+    
+}

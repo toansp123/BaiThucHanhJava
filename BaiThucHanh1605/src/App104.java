929a50cb260e0dadcb040acaa97dbda28cc9ea29
toansp123 <97439368+toansp123@users.noreply.github.com>
2022-05-18 10:02:13 +0700

    Delete BaiThucHanh1605 directory

--- BaiThucHanh1605/src/App104.java
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-public class App106 {
-    
-}
-=======
-public class App106 {
-    
-}
->>>>>>> ced51766c0cccc1ab1156833d83a6f1e37c1d9aa
--- BaiThucHanh1605/src/App96.java
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-public class App98 {
-    
-}
-=======
-public class App98 {
-    
-}
->>>>>>> ced51766c0cccc1ab1156833d83a6f1e37c1d9aa

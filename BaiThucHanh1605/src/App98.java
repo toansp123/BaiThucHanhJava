9d28f414838043f7dc2cdb2a4c2dabfac06781c8
toansp123 <whuytoan123@gmail.com>
2022-05-18 10:29:11 +0700

    BaiThucHanh1605

---
+++ BaiThucHanh1605/src/App98.java
@@ -0,0 +1,16 @@
+import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class App96 {
+    public static void main(String[] args) {
+
+        TreeMap<String, Double> treeMap1 = new TreeMap<>();
+       
+        Map<Float, Double> map = new HashMap<>();
+
+        TreeMap<Float, Integer> treeMap2 =new TreeMap<>();
+    }
+    
+}
